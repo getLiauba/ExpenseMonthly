21cd9f3c037831a575ab46e642d4d4d9d97acb80
Andrew Liauba <Iamliauba@hotmail.com>
2023-02-19 14:42:21 -0500

    Initial Commit

---
+++ ExpenseMonthly/ExpenseMonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseMonthlyApp.swift
+//  ExpenseMonthly
+//
+//  Created by andrew liauba on 2023-02-19.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseMonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
