292134ccb5ab60bf7b6411dbeec7333a0db69fd3
Andrew Liauba <Iamliauba@hotmail.com>
2023-03-20 09:23:40 -0400

    Add geometry reader in bar graph to ensure the chart will stay within frame

--- ExpenseMonthly/Views/GraphView.swift
+++ ExpenseMonthly/Views/GraphView.swift
@@ -12,13 +12,14 @@ struct GraphView: View {
     @StateObject var vm: CoreDataViewModel
     @State var barHeights: [CGFloat]
     
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 Spacer()
                 HStack {
                     ForEach(barHeights.indices) { index in
-                        GraphBar(barHeight: $barHeights[index], month: getMonthLabel(5 - index))
+                        GraphBar(barHeight: $barHeights[index], month: getMonthLabel(5 - index),maxHeight: getMaxHeight(barHeights: barHeights), minHeight: getMinHeight(barHeights: barHeights))
                     }
                 }
                 .padding(.leading)
@@ -30,6 +31,19 @@ struct GraphView: View {
             barHeights = vm.getTrailingMonthTotals()
         }
     }
+    
+    func getMaxHeight(barHeights: [CGFloat]) -> CGFloat {
+        
+        let barHeights = barHeights.sorted()
+        return barHeights[barHeights.count-1]
+    }
+    
+    func getMinHeight(barHeights: [CGFloat]) -> CGFloat {
+        
+        let barHeights = barHeights.sorted()
+        return barHeights[0]
+    }
+    
     
     func getMonthLabel(_ index: Int) -> String {
         let calendar = Calendar.current
@@ -45,27 +59,31 @@ struct GraphBar: View {
     @Binding var barHeight: CGFloat
     @State var barHeightStart = 0.0
     var month: String
+    @State var maxHeight: CGFloat
+    @State var minHeight: CGFloat
     
     var body: some View {
-        VStack {
-            Spacer()
+        GeometryReader { geometry in
             VStack {
-                Text("$\(Int(barHeight))")
-                    .font(.caption)
-                    .fontWeight(.heavy)
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 20, height: barHeightStart)
-                    .padding()
-                    .foregroundColor(.green)
-                    .opacity(0.6)
-                    .onAppear() {
-                        withAnimation(.linear(duration: 1.0)) {
-                            barHeightStart = barHeight
+                Spacer()
+                VStack {
+                    Text("$\(Int(barHeight))")
+                        .font(.caption)
+                        .fontWeight(.heavy)
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 20, height: barHeightStart)
+                        .padding()
+                        .foregroundColor(.green)
+                        .opacity(0.6)
+                        .onAppear() {
+                            withAnimation(.linear(duration: 1.0)) {
+                                barHeightStart = barHeight
+                            }
                         }
-                    }
-                Text(month)
-                    .font(.caption)
-                    .fontWeight(.heavy)
+                    Text(month)
+                        .font(.caption)
+                        .fontWeight(.heavy)
+                }
             }
         }
     }
